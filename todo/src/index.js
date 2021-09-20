d79fae744e049ac3e53c0340bc118ac436264ccd
volha <v.khviadchenia@gmail.com>
2021-09-20 17:41:21 +0200

    refactor: create file for component App

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,30 +1,6 @@
 import React from 'react';
 import reactDom from 'react-dom';
 
-import AppHeader from './components/app-header/app-header';
-import SearchPanel from './components/search-panel/search-panel';
-import ItemStatusFilter from './components/item-status-filter/item-status-filter';
-import ToDoList from './components/todo-list/todo-list';
-
-import './index.css'
-
-const App = () => {
-
-  const todoData = [
-    {label: 'Drink Coffee', important: false, key: 1},
-    {label: 'Build React App', important: true, key: 2},
-    {label: 'Have a lunch', important: false, key: 3},
-  ]
-  return (
-    <div className='todo-app'>
-    <AppHeader toDo={1} done={3} />
-    <div className="top-panel d-flex">
-    <SearchPanel />
-    <ItemStatusFilter />
-    </div>
-    <ToDoList todos = { todoData }/>
-  </div>
-  )
-};
+import App from './components/app/app'
 
 reactDom.render(<App />, document.getElementById('root'));
