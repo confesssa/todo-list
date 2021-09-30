e495ac2abec3c95601ce37f67abcbb410f95ee2f
volha <v.khviadchenia@gmail.com>
2021-09-30 17:38:28 +0200

    refactor: rewrite two components on class-components

--- todo/src/components/item-status-filter/item-status-filter.js
+++ todo/src/components/item-status-filter/item-status-filter.js
@@ -1,19 +1,20 @@
-import './item-status-filter.css';
+import { Component } from "react";
+import "./item-status-filter.css";
 
-const itemStatusFilter = () => {
-  return (
-    <div className='btn-group'>
-      <button type='button'
-              className='btn btn-info'>All
-      </button>
-      <button type='button'
-              className='btn btn-outline-secondary'>Active
-      </button>
-      <button type='button'
-              className='btn btn-outline-secondary'>Done
-      </button>
-    </div>
-  )
+export default class itemStatusFilter extends Component {
+  render() {
+    return (
+      <div className="btn-group">
+        <button type="button" className="btn btn-info">
+          All
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Active
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Done
+        </button>
+      </div>
+    );
+  }
 }
-
-export default itemStatusFilter;
--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -1,29 +1,34 @@
-import './todo-list-item.css';
+import "./todo-list-item.css";
+import React, { Component } from "react";
 
-const ToDoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
+export default class ToDoListItem extends Component {
+  render() {
+    const { label, important = false } = this.props;
+    const style = {
+      color: important ? "steelblue" : "black",
+      fontWeight: important ? "bold" : "normal",
+    };
 
-  return (
-    <span className='todo-list-item'>
-      <span className='todo-list-item-label'
-            style={ style }>
-        { label }
+    return (
+      <span className="todo-list-item">
+        <span className="todo-list-item-label" style={style}>
+          {label}
+        </span>
+
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-end"
+        >
+          <i className="fas fa-exclamation"></i>
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-end"
+        >
+          <i className="fas fa-trash"></i>
+        </button>
       </span>
-
-      <button type='button'
-              className='btn btn-outline-success btn-sm float-end'>
-                <i className='fas fa-exclamation'></i>
-      </button>
-
-      <button type='button'
-              className='btn btn-outline-danger btn-sm float-end'>
-                <i className='fas fa-trash'></i>
-      </button>
-    </span>
-  )
-};
-
-export default ToDoListItem;
+    );
+  }
+}
