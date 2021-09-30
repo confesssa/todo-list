ce3dd217afdbc4ebac8e3e321ae4ceff69151a9d
volha <v.khviadchenia@gmail.com>
2021-10-01 01:25:58 +0200

    feat: add method onMarkImportant

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -2,30 +2,50 @@ import "./todo-list-item.css";
 import React, { Component } from "react";
 
 export default class ToDoListItem extends Component {
+  state = {
+    done: false,
+    important: false,
+  };
+
   onLabelClick = () => {
-    console.log(`Done: ${this.props.label}`);
+    if (this.state.done) {
+      this.setState({ done: false });
+    } else {
+      this.setState({ done: true });
+    }
+  };
+
+  onMarkImportant = () => {
+    if (this.state.important) {
+      this.setState({
+        important: false,
+      });
+    } else {
+      this.setState({ important: true });
+    }
   };
 
   render() {
-    const { label, important = false } = this.props;
-    const style = {
-      color: important ? "steelblue" : "black",
-      fontWeight: important ? "bold" : "normal",
-    };
+    const { label } = this.props;
+    const { done, important } = this.state;
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += " done";
+    }
+    if (important) {
+      classNames += " important";
+    }
 
     return (
-      <span className="todo-list-item">
-        <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={this.onLabelClick}
-        >
+      <span className={classNames}>
+        <span className="todo-list-item-label" onClick={this.onLabelClick}>
           {label}
         </span>
 
         <button
           type="button"
           className="btn btn-outline-success btn-sm float-end"
+          onClick={this.onMarkImportant}
         >
           <i className="fas fa-exclamation"></i>
         </button>
--- todo/src/components/todo-list/todo-list.js
+++ todo/src/components/todo-list/todo-list.js
@@ -1,22 +1,16 @@
 import ToDoListItem from "../todo-list-item/todo-list-item";
-import './todo-list.css';
+import "./todo-list.css";
 
 const ToDoList = ({ todos }) => {
   const elements = todos.map((item) => {
-    const { id, ...itemProps} = item;
+    const { id, ...itemProps } = item;
     return (
-      <li key = { id } className='list-group-item'>
-      <ToDoListItem 
-      {...itemProps}
-      />
-    </li>
-    )
-  })
-  return (
-    <ul className='list-group todo-list'>
-      { elements }
-    </ul>
-  )
+      <li key={id} className="list-group-item">
+        <ToDoListItem {...itemProps} />
+      </li>
+    );
+  });
+  return <ul className="list-group todo-list">{elements}</ul>;
 };
 
 export default ToDoList;
