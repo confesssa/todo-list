ddd0170c901017ce5eb414a87fc7fcdb2af6bdce
volha <v.khviadchenia@gmail.com>
2021-10-05 23:37:54 +0200

    feat: add function for task  adding

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -2,11 +2,14 @@ import AppHeader from "../app-header/app-header";
 import SearchPanel from "../search-panel/search-panel";
 import ItemStatusFilter from "../item-status-filter/item-status-filter";
 import ToDoList from "../todo-list/todo-list";
+import ItemAddForm from "../item-add-form/item-add-form";
 
 import "./app.css";
 import React, { Component } from "react";
 
 export default class App extends Component {
+
+  maxId = 100;
   state = {
     todoData: [
       { label: "Drink Coffee", important: false, id: 1 },
@@ -14,6 +17,23 @@ export default class App extends Component {
       { label: "Have a lunch", important: false, id: 3 },
     ],
   };
+
+  addItem = (text) => {
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    }
+
+    this.setState (({ todoData }) => {
+      const newArray = [...todoData, newItem];
+      return {
+        todoData: newArray
+      }
+
+    })
+    
+  }
 
   deleteItem = (id) => {
     this.setState(({ todoData }) => {
@@ -30,11 +50,15 @@ export default class App extends Component {
     return (
       <div className="todo-app">
         <AppHeader toDo={1} done={3} />
+
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter />
         </div>
+        
         <ToDoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+
+        <ItemAddForm onItemAdded={ this.addItem } />
       </div>
     );
   }
---
+++ todo/src/components/item-add-form/item-add-form.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import './item-add-form.css';
+
+
+export default class ItemAddForm extends Component {
+  render() {
+    return (
+      <div className="item-add-form">
+        <button className="btn btn-outline-secondary" 
+        type='button' 
+        onClick={() => this.props.onItemAdded('Hello World')}>
+          Add Item
+        </button>
+      </div>
+    )
+  }
+}
