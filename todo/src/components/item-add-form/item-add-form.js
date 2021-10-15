dcb382fa4234a8f9fa3a819e0c10b7a172366aee
volha <v.khviadchenia@gmail.com>
2021-10-15 12:19:08 +0200

    feat: add form handing

--- todo/src/components/item-add-form/item-add-form.js
+++ todo/src/components/item-add-form/item-add-form.js
@@ -3,15 +3,32 @@ import './item-add-form.css';
 
 
 export default class ItemAddForm extends Component {
+
+  state = {
+    label: ''
+  }
+
+  onLabelChange = (e) =>{
+    this.setState({
+      label: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  }
+
   render() {
     return (
-      <div className="item-add-form">
-        <button className="btn btn-outline-secondary" 
-        type='button' 
-        onClick={() => this.props.onItemAdded('Hello World')}>
+      <form className="item-add-form d-flex"
+            onSubmit={this.onSubmit}>
+        <input type="text" className='form-control' onChange={this.onLabelChange} placeholder="What needs to be done" />
+        <button className="btn btn-outline-secondary form-button" 
+        type='submit' >
           Add Item
         </button>
-      </div>
+      </form>
     )
   }
 }
