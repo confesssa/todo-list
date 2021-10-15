047b4e868b7b214df2ae30a0d36c8aab5e8bf40c
volha <v.khviadchenia@gmail.com>
2021-10-15 12:39:26 +0200

    feat: update input to controlled element

--- todo/src/components/item-add-form/item-add-form.js
+++ todo/src/components/item-add-form/item-add-form.js
@@ -17,13 +17,20 @@ export default class ItemAddForm extends Component {
   onSubmit = (e) => {
     e.preventDefault();
     this.props.onItemAdded(this.state.label);
+    this.setState({
+      label: ""
+    })
   }
 
   render() {
     return (
       <form className="item-add-form d-flex"
             onSubmit={this.onSubmit}>
-        <input type="text" className='form-control' onChange={this.onLabelChange} placeholder="What needs to be done" />
+        <input type="text" 
+               className='form-control'
+               onChange={this.onLabelChange} 
+               placeholder="What needs to be done"
+               value={this.state.label} />
         <button className="btn btn-outline-secondary form-button" 
         type='submit' >
           Add Item
